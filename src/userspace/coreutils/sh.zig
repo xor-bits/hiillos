7083973a33c2fb810612076911041dcdbb649c0d
xor-bits <oneleh@utu.fi>
2025-07-07 18:36:33 +0300

    feat(coreutils/sh): execute commands with args

--- src/userspace/coreutils/sh.zig
+++ src/userspace/coreutils/sh.zig
@@ -28,18 +28,8 @@ pub fn main(ctx: @import("main.zig").Ctx) !void {
         const cmd = command[0..command_len];
         command_len = 0;
 
-        const args = try caps.Frame.create(0x1000);
-        errdefer args.close();
-
-        var args_stream = args.stream();
-        try std.fmt.format(
-            args_stream.writer(),
-            "initfs:///sbin/{s}",
-            .{cmd},
-        );
-
         const result = try abi.lpc.call(abi.PmProtocol.ExecElfRequest, .{
-            .arg_map = args,
+            .arg_map = try createArgMap(cmd),
             .env_map = try caps.Frame.create(0x1000),
             .stdio = try @import("main.zig").stdio.clone(),
         }, .{
@@ -71,4 +61,28 @@ pub fn main(ctx: @import("main.zig").Ctx) !void {
             );
         }
     }
+}
+
+fn createArgMap(cli: []const u8) !caps.Frame {
+    const args = try caps.Frame.create(0x1000);
+    errdefer args.close();
+
+    var args_stream = args.stream();
+    var args_buffered_stream = std.io.bufferedWriter(args_stream.writer());
+    var args_writer = args_buffered_stream.writer();
+
+    // TODO: use $PATH instead
+    try args_writer.writeAll("initfs:///sbin/");
+
+    var input_arg_it = std.mem.splitScalar(u8, cli, ' ');
+    while (input_arg_it.next()) |input_arg| {
+        if (input_arg.len == 0) continue;
+
+        try args_writer.writeAll(input_arg);
+        try args_writer.writeAll(&.{0});
+    }
+
+    try args_buffered_stream.flush();
+
+    return args;
 }
